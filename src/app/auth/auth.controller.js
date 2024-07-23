6ade4d89e082a6b515a34b2171b6a886d487a628
itsMePujan <69003888+itsMePujan@users.noreply.github.com>
2024-07-23 16:24:38 +0545

    zod validation

--- src/app/auth/auth.controller.js
+++ src/app/auth/auth.controller.js
@@ -1,8 +1,20 @@
+const { z } = require("zod");
+
 class authController {
   //register function
   register = (req, res, next) => {
     try {
       let payload = req.body;
+
+      const register = z.object({
+        name: z.string().min(2).max(40),
+        email: z.string().email(),
+        role: z
+          .string()
+          .regex(/admin|user|hosteler/)
+          .default("user"),
+      });
+      let validatedData = register.parse(payload);
 
       if (req.file) {
         payload.image = req.file.filename;
